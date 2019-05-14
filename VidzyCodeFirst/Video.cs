8c29ad306d2fbb942340b7f71306c4410a48da49
Devi <devidze.giorgi@gmail.com>
2019-05-14 22:21:27 +0400

    Add project files.

---
+++ VidzyCodeFirst/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidzyCodeFirst
+{
+    class Video
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+
+    }
+}
