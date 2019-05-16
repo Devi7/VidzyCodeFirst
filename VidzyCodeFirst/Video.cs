bc22a86e3eb8d88099269a06dec0526f5865ae3e
Devi <devidze.giorgi@gmail.com>
2019-05-16 22:58:15 +0400

    update models and databases

--- VidzyCodeFirst/Video.cs
+++ VidzyCodeFirst/Video.cs
@@ -7,13 +7,22 @@ using System.Threading.Tasks;
 
 namespace VidzyCodeFirst
 {
+
+    public enum Classification
+    {
+        Silver = 1,
+        Gold = 2,
+        Platinum = 3
+    }
+
     public class Video
     {
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
-        public IList<Genre> Genres { get; set; }
+        public Genre Genre { get; set; }
+        public Classification Classification { get; set; }
 
     }
 }
