e0eaf1be8ec9284cc02f65fe97dacbb7a8f9132f
Devi <devidze.giorgi@gmail.com>
2019-05-30 23:52:57 +0400

    add configuration for db

---
+++ VidzyCodeFirst/EntityConfiguration/VideoConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidzyCodeFirst.EntityConfiguration
+{
+    public class VideoConfiguration : EntityTypeConfiguration<Video>
+    {
+
+        public VideoConfiguration()
+        {
+            ToTable("Videos");
+            HasKey(v => v.Id);
+
+            Property(v => v.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            Property(v => v.Genre)
+        }
+
+
+    }
+}
